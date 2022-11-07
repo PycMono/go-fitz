90b41f9e00838f3f53387a7d35c1668f805efc34
彭亚川 <pengyc@dustess.com>
2022-11-07 23:42:49 +0800

    fix: 🐛 修复内存泄露

--- fitz.go
+++ fitz.go
@@ -224,7 +224,7 @@ func (f *Document) ImageDPI(pageNumber int, dpi float64) (image.Image, error) {
 	}
 
 	img.Pix = C.GoBytes(unsafe.Pointer(pixels), C.int(4*bbox.x1*bbox.y1))
-	defer C.CFRelease(C.CFTypeRef(pixels))
+	defer C.free(unsafe.Pointer(pixels))
 	img.Rect = image.Rect(int(bbox.x0), int(bbox.y0), int(bbox.x1), int(bbox.y1))
 	img.Stride = 4 * img.Rect.Max.X
 
